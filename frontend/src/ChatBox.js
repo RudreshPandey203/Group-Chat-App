0918c6f27f59b5e727e186a1611f6bf49a0978ca
Rudresh Pandey <pandeyrudresh203@gmail.com>
2024-02-14 21:47:10 +0530

    halfway css

--- frontend/src/ChatBox.js
+++ frontend/src/ChatBox.js
@@ -2,69 +2,95 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 
 function ChatBox({ socket, username }) {
-    const [currentMessage, setCurrentMessage] = useState("");
-    const [messages, setMessages] = useState([]);
-    const [prevmssg, setPrevmssg] = useState("");
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [prevmssg, setPrevmssg] = useState("");
+  const [userList, setUserList] = useState([]);
 
-    const sendMessage = async() => {
-        if (currentMessage && currentMessage !== "") {
-            const messageData = {
-                username,
-                message: currentMessage,
-                time : new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes() + ":" + new Date(Date.now()).getSeconds()
-            };
+  const sendMessage = async () => {
+    if (currentMessage && currentMessage !== "") {
+      const messageData = {
+        username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes() +
+          ":" +
+          new Date(Date.now()).getSeconds(),
+      };
 
-            setMessages((messages) => [...messages, messageData]);
+      setMessages((messages) => [...messages, messageData]);
 
-            await socket.emit("chat_message", JSON.stringify(messageData));
-            console.log("sent message : ",messageData);
-            setCurrentMessage("");
-        }
+      await socket.emit("chat_message", JSON.stringify(messageData));
+      console.log("sent message : ", messageData);
+      setCurrentMessage("");
     }
+  };
 
-    // useEffect(() => {
-    //     socket.on("chat_message_recieve", (msg) => {
-    //         const messageData = JSON.parse(msg);
-    //         console.log("recieved message : ",messageData);
-    //         setMessages((messages) => [...messages, messageData]);
-    //         console.log(messages)
-    //     });
-    // }, []);
-    useEffect(() => {
-        const handleReceivedMessage = (msg) => {
-            const messageData = JSON.parse(msg);
-            setMessages((prevMessages) => [...prevMessages, messageData]);
-        };
+  useEffect(() => {
+    const handleUserList = (userList) => {
+      console.log("user list", userList);
+      setUserList(JSON.parse(userList));
+    };
 
-        socket.on("chat_message_recieve", handleReceivedMessage);
+    socket.on("user_list", handleUserList);
 
-        return () => {
-            socket.off("chat_message_recieve", handleReceivedMessage);
-        };
-    }, [socket]);
+    return () => {
+      socket.off("user_list", handleUserList);
+    };
+  }, [socket]);
+
+  useEffect(() => {
+    const handleReceivedMessage = (msg) => {
+      const messageData = JSON.parse(msg);
+      setMessages((prevMessages) => [...prevMessages, messageData]);
+    };
+
+    socket.on("chat_message_recieve", handleReceivedMessage);
+
+    return () => {
+      socket.off("chat_message_recieve", handleReceivedMessage);
+    };
+  }, [socket]);
 
   return (
-    <div className="chatBox">
-      <div className="head">
-        <h1>Live Chat</h1>
-      </div>
-      <div className="body">
-        {messages.map((message, index) => {
+    <div className="chatBox flex ">
+      <div className="w-[15vw] h-[90vh] bg-gray-800 p-3">
+        <h1 className="text-2xl text-white">Users</h1>
+        {userList.map((user, index) => {
+          return (
+            <div key={index} className="text-white">
+              <p>{user.username}</p>
+            </div>
+          );
+        })}
+        </div>
+      <div>
+        <div className="head">
+          <h1>Live Chat</h1>
+        </div>
+        <div className="body">
+          {messages.map((message, index) => {
             return (
-                <div key={index} className="message">
+              <div key={index} className="message">
                 <h3>{message.username}</h3>
                 <p>{message.message}</p>
                 <p>{message.time}</p>
-                </div>
+              </div>
             );
-        }
-        )}
-      </div>
-      <div className="footer">
-        <input name="message" placeholder="Type your message" onChange={(event)=>{
-            setCurrentMessage(event.target.value);
-        }} />
-        <button onClick={sendMessage}>Send</button>
+          })}
+        </div>
+        <div className="footer">
+          <input
+            name="message"
+            placeholder="Type your message"
+            onChange={(event) => {
+              setCurrentMessage(event.target.value);
+            }}
+          />
+          <button onClick={sendMessage}>Send</button>
+        </div>
       </div>
     </div>
   );
---
+++ frontend/src/Join.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import io from "socket.io-client";
+import ChatBox from "./ChatBox";
+
+const socket = io("http://localhost:4000");
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [chat, setChat] = useState(false);
+  const [userError, setUserError] = useState(false);
+
+  const joinChat = () => {
+    if (username.trim() !== "") {
+      socket.emit("join_chat", username);
+      console.log(`User ${username} joined the chat`);
+      setChat(true);
+    } else {
+      setUserError(true);
+      setTimeout(() => {
+        setUserError(false);
+      }, 3000);
+    }
+  };
+
+  return (
+    <div className="bg-black h-[100vh] text-white ">
+      <div className="flex justify-between p-3">
+        <h1 className="p-2 text-4xl">Chat App</h1>
+        {chat && <button className="m-3 bg-red-600 p-2 rounded-md" onClick={() => { setChat(false); }}>Leave Chat</button>
+}
+      </div>
+      {!chat && <div className="h-[80vh] text-center flex flex-col justify-center items-center">
+        <h1 className="p-5 text-5xl">Enter your Username</h1>
+
+        <div>
+          <input className="rounded-md p-2 m-3 w-[20vw] h-16 text-gray-700 text-3xl font-semibold" type="text"
+            name="username"
+            placeholder="John"
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+            required
+          />
+          {userError && (
+            <p className="text-red-500 p-1 float">Please enter a valid username</p>
+          )}
+          <div>
+            <button className="rounded-md bg-blue-700 px-6 py-4 m-3 text-2xl hover:bg-blue-500 font-semibold" onClick={joinChat}>Join Chat</button>
+          </div>
+        </div>
+      </div>}
+      {chat && <div>
+        <ChatBox socket={socket} username={username} />
+      </div>}
+    </div>
+  );
+}
+
+export default App;
