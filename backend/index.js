1e9f9cadf91615bf6e739665f66b26d6f30bc7a6
Rudresh Pandey <pandeyrudresh203@gmail.com>
2024-02-15 19:32:39 +0530

    site works

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const users = [];
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://group-chat-app-ruddy.vercel.app/",
         methods: ["GET", "POST"]
     }
 });
